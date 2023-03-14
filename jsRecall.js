ac034d925f71b00a987176ba08bfda9040ebe487
Jenna Perez <jmsp43@gmail.com>
2023-03-14 09:54:46 -0400

    first commit

---
+++ jsRecall.js
@@ -0,0 +1,148 @@
+// How do we assign a value to a variable?
+
+//let variable = assigned value
+
+
+
+
+// How do we change the value of a variable?
+
+//let variable = new assigned value
+
+
+
+
+// How do we assign an existing variable to a new variable?
+
+//let new variable = old variable
+
+
+
+
+// Remind me, what are declare, assign, and define?
+
+//declare: let or const or var, no value assigned yet
+//assign: variable = assigned value, = is the assignment
+//define:declaring and assigning a variable at the same time
+
+
+
+
+// What is pseudocoding and why should you do it?
+
+//Writing out your logic in plain english. A way to plan out your code line by line.
+
+
+
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+//at least 10% of the time you have to spend should be thinking through and planning out the problem by writing psuedocode
+
+
+
+
+// Create a variable called firstVariable
+
+//let firstVariable
+
+
+
+
+// Assign it the value of the string "Hello World"
+
+//firstVariable = "Hello World"
+
+
+
+
+// Change the value of this variable to some number
+
+//firstVariable = 9
+
+
+
+
+// Store the value of firstVariablein a new variable called secondVariable
+
+//let secondVariable = firstVariable
+
+
+
+
+
+// Change the value of secondVariableto any string.
+
+//secondVariable = "some string"
+
+
+
+
+
+// What is the value of firstVariable?
+
+//9
+
+
+
+// Create a variable called yourNameand set it equal to your name as a string. 
+
+//let yourName = 'Jenna'
+
+
+
+
+//Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+
+//let greetings = 'Hello, my name is '
+//console.log(greetings+yourName)
+
+
+
+
+
+
+// Using the provided variable definitions, replace the blanks so that all log statements print truein the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print trueto the console
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false || false || false || false || false || true);
+// console.log(false === false)
+// console.log(e === 'Kevin');
+// console.log(a != b != c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 == '48');
+
+
+
+
+// Declare a variable animal. Set it to be either "cow" or something else
+
+//let animal = 'cow'
+
+// Write code that will print out "mooooo" if the it is equal to cow
+
+if (animal === 'cow') {
+    console.log('mooooo')
+}
+// Change your code so that if the variable animalis anything other than a cow, it will print "Hey! You're not a cow."
+
+if (animal != 'cow') {
+    console.log('Hey! you are not a cow')
+}
+
+// Commit
+//git init
+//git add .
+//git commit
+
+
